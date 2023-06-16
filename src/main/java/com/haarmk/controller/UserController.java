1ba4179786a558027224bf25485df024769b6354
atulsinghmbbs <atulbhadouria53@gmail.com>
2023-06-16 17:01:16 +0530

    added init

---
+++ src/main/java/com/haarmk/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.haarmk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping
+	public String name() {
+		return "Hello World";
+	}
+
+}
